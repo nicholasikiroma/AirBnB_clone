317ebe2b803d2373da825b7c40257c36b2028994
Getachew7 <69458198+Getachew7@users.noreply.github.com>
2022-11-23 09:57:35 -0800

    updated base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """Module contains superclass for mananging all Airbnb objects"""
 import uuid
 import datetime
+import models
 
 
 class BaseModel:
@@ -35,6 +36,7 @@ class BaseModel:
     def save(self):
         """updates the public instance attribute updated_at with the current datetime"""
         self.updated_at = datetime.datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
