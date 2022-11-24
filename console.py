71e1dbab68e7718878fd4acea13ddf19ca5d6762
Nicholas <nicholasikiroma@gmail.com>
2022-11-24 08:22:38 +0100

    added more commands for console

--- console.py
+++ console.py
@@ -35,6 +35,76 @@ class HBNBCommand(cmd.Cmd):
             new_instance.save()
             print(new_instance.id)
 
+    def do_show(self, line):
+        """Prints the string representation of an instance
+           Args:
+                line: CLI arguments
+        """
+        arg = line.split(' ')
+
+        if line:
+
+            if arg[0] in models.default_classes:
+                if len(arg) > 1:
+                    key = f"{arg[0]}.{arg[1]}"
+                    try:
+                        print(models.storage.all()[key])
+
+                    except:
+                        print("** no instance found **")
+                else:
+                    print("** instance id missing **")
+            else:
+                print("** class doesn't exist **")
+        else:
+            print("** class name missing **")
+
+    def do_destroy(self, line):
+        """ Deletes an instance based on the class name and id
+           Args:
+                line: CLI arguments
+        """
+        arg = line.split(' ')
+
+        if line:
+
+            if arg[0] in models.default_classes:
+                if len(arg) > 1:
+                    key = f"{arg[0]}.{arg[1]}"
+                    try:
+                        models.storage.all().pop(key)
+                        models.storage.save()
+                    except:
+                        print("** no instance found **")
+                else:
+                    print("** instance id missing **")
+            else:
+                print("** class doesn't exist **")
+        else:
+            print("** class name missing **")
+
+    def do_all(self, line):
+        """Prints all string representation of all instances"""
+        
+        all_obj = []
+
+        if line:
+            arg = line.split()
+            if arg[0] in models.default_classes:
+                for key, instance in models.storage.all().items():
+                    if key.split('.')[0] == arg[0]:
+                        all_obj.append(str(instance))
+
+            else:
+                print("** class doesn't exist **")
+
+        else:
+            for key, instance in models.storage.all().items():
+                all_obj.append(str(instance))
+        
+        if all_obj:
+            print(all_obj)
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Module for FileStorage class."""
 
-import datetime
 import json
 import os
 import models
