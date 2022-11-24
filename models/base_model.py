3fbb71547797d9f52fb6e54370c8e3ba40ee3021
Nicholas <nicholasikiroma@gmail.com>
2022-11-24 20:31:53 +0100

    more bug fixes

--- models/base_model.py
+++ models/base_model.py
@@ -5,27 +5,31 @@ import datetime
 import models
 
 
-class BaseModel:
-    """Handles initialization, serialization and deserialization of your future instances
+class BaseModel():
+    """Handles initialization, serialization and
+        deserialization of your future instances
        Args:
             *args: arguments(unused)
-            **kwargs(dict): attrubute values   
+            **kwargs(dict): attrubute values
     """
     def __init__(self, *args, **kwargs):
         """Initialization of class instances"""
         DATE_TIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%f'
+
         if not kwargs:
             user_id = uuid.uuid4()
             self.id = str(user_id)
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
             models.storage.new(self)
+
         else:
+
             for key, value in kwargs.items():
                 if key == "__class__":
                     continue
 
-                if key in ("updated_at", "created_at"):
+                elif key in ("updated_at", "created_at"):
                     self.__dict__[key] = datetime.datetime.strptime(
                         value, DATE_TIME_FORMAT)
                 elif key[0] == "id":
@@ -35,16 +39,19 @@ class BaseModel:
 
     def __str__(self):
         """Override string representation of class instance"""
-        return f"[{__class__.__name__}] {self.id} {self.__dict__}"
+        return f"[{self.__class__.__name__}] {self.id} {self.__dict__}"
 
     def save(self):
-        """updates the public instance attribute updated_at with the current datetime"""
+        """
+        updates the public instance attribute,
+        updated_at with the current datetime
+        """
         self.updated_at = datetime.datetime.now()
         models.storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
-        my_dict = {"__class__": __class__.__name__}
+        my_dict = {"__class__": self.__class__.__name__}
 
         for key in self.__dict__:
             if key in ("created_at", "updated_at"):
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,45 +6,42 @@ import json
 import os
 import models
 
+
 class FileStorage:
     """class for serialization and deserialization of base classes."""
     __file_path = "file.json"
     __objects = {}
-    
+
     def all(self):
         """Returns __objects dict."""
 
-        return FileStorage.__objects
-        
+        return self.__objects
+
     def new(self, obj):
         """Sets new obj in __objects dictionary."""
 
         key = "{}.{}".format(type(obj).__name__, obj.id)
-        FileStorage.__objects[key] = obj
-        
+        self.__objects[key] = obj
+
     def save(self):
         """Serializes __objects to JSON file."""
         temp = {}
-        for id, obj in FileStorage.__objects.items():
+        for id, obj in self.__objects.items():
             temp[id] = obj.to_dict()
-        
-        with open(FileStorage.__file_path, "w") as json_file:
+
+        with open(self.__file_path, "w") as json_file:
             json.dump(temp, json_file)
 
-       # with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
-        #    d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
-         #   json.dump(d, f)
-            
     def reload(self):
         """Deserializes JSON file into __objects."""
 
-        if not os.path.isfile(FileStorage.__file_path):
+        if not os.path.isfile(self.__file_path):
             return
 
-        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+        with open(self.__file_path, "r", encoding="utf-8") as f:
             obj_dict = json.load(f)
 
-            for k, v in obj_dict.items():
-                obj_dict_instance = models.default_classes[v["__class__"]](**v)
+        for k, v in obj_dict.items():
+            obj_dict_instance = models.default_classes[v["__class__"]](**v)
 
-            FileStorage.__objects[k] = obj_dict_instance
+            self.__objects[k] = obj_dict_instance
