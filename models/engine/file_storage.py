bc24b5907ec0ad0a8c35635755bc11101b477eff
Getachew7 <69458198+Getachew7@users.noreply.github.com>
2022-11-24 10:30:15 -0800

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,12 +5,7 @@ import datetime
 import json
 import os
 import models
-from models.user import User
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
+
 
 class FileStorage:
     """class for serialization and deserialization of base classes."""
