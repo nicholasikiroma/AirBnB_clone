5b15eb53c0b2e2e983dd02dbd073b3a046115838
Nicholas <nicholasikiroma@gmail.com>
2022-11-24 10:40:48 +0100

    updated init

--- models/__init__.py
+++ models/__init__.py
@@ -2,8 +2,14 @@
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.user import User
+from models.amenity import Amenity
+from models.place import Place
+from models.city import City
+from models.review import Review
+from models.state import State
 
-default_classes = {"BaseModel": BaseModel, "User":User}
+default_classes = {"BaseModel": BaseModel, "User":User, "State": State, "City": City,
+                    "Amenity": Amenity, "Place": Place, "Review": Review}
 
 storage = FileStorage()
 storage.reload()
