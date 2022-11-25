de9aa8f802672fb289d4e91fc8e98906a685e3fe
Getachew7 <69458198+Getachew7@users.noreply.github.com>
2022-11-25 02:39:01 -0800

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,9 @@
 """Module to create unique FileStorage."""
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
+from mosels.user import User
 
-default_classes = {"BaseModel": BaseModel}
+default_classes = {"BaseModel": BaseModel, "User":User}
 
 storage = FileStorage()
 storage.reload()
