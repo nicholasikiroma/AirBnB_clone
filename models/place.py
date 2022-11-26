b5a071207fd3a9088a3ae4c0deeab4d591c57af1
Getachew7 <69458198+Getachew7@users.noreply.github.com>
2022-11-26 09:34:39 -0800

    Update place.py

--- models/place.py
+++ models/place.py
@@ -18,7 +18,6 @@ class Place(BaseModel):
         longitude (float): The longitude of the place.
         amenity_ids (list): A list of Amenity ids.
     """
-    
     city_id = ""
     user_id = ""
     name = ""
