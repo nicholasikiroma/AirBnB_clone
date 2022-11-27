959c9a0abf215a5cf9b7309e1c2c79fc64aacea3
Nicholas <nicholasikiroma@gmail.com>
2022-11-27 01:56:00 +0100

    fixing

--- models/base_model.py
+++ models/base_model.py
@@ -28,7 +28,8 @@ class BaseModel():
                     setattr(self, key, kwargs[key])
 
         else:
-            self.id = str(uuid.uuid4)
+            user_id = uuid.uuid4()
+            self.id = str(user_id)
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
             models.storage.new(self)
